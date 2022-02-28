b003e8f0eef3fe7e1e385aa254cacf31a8e4ea30
keyur <keyurkhakhkhar007@gmail.com>
2022-02-28 12:14:15 +0530

    Adding project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { NodeList } from "./components/node-list/node-list.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      notes: [
+        {
+          noteName: "Today's Set-Up",
+          noteId: "n1",
+          isActive: "",
+        },
+        {
+          noteName: "Today's agenda",
+          noteId: "n2",
+          isActive: "",
+        },
+        {
+          noteName: "yesterday's work review",
+          noteId: "n3",
+          isActive: "",
+        },
+        {
+          noteName: "Learning Topics",
+          noteId: "n4",
+          isActive: "",
+        },
+        {
+          noteName: "Implementations",
+          noteId: "n5",
+          isActive: "",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NodeList>
+          {this.state.notes.map((note) => (
+            <h1 key={note.noteId}>
+              {note.noteName}
+              {note.isActive}
+            </h1>
+          ))}
+        </NodeList>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/node-list/node-list.component.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NodeList = (props) => {
+  return <div>{props.children}</div>;
+};
